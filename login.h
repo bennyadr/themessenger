ff22bed827267a6faedfeec8e09f48c35b11388c
biotech86 <biotech86@3b6a47cb-6844-0410-8745-ed9a1b79f187>
2008-02-10 15:48:08 +0000

    nu se compileaza ...

---
+++ login.h
@@ -0,0 +1,25 @@
+#ifndef _LOGIN_H
+#define _LOGIN_H
+
+#include "action.h"
+#include "ypacket.h"
+#include "socket.h"
+
+using namespace Action;
+using namespace YPacket;
+ 
+class c_Login : public c_Action
+{
+	public:
+		c_Login();
+		~c_Login();
+		void Execute();
+	private:
+		void CreateLoginPacket();
+		void SendPacket();
+	private:
+		c_Socket *m_cSocket;
+		c_YPacket *m_cPacket;
+};
+
+#endif
